f9f7fbd998458116a2c8b4fc3b99e64d3ab45aa2
Amiola Demilade <amiolademilade@gmail.com>
2025-05-10 14:44:31 +0100

    text

--- src/components/Frontlett/HeroSection.tsx
+++ src/components/Frontlett/HeroSection.tsx
@@ -23,7 +23,7 @@ const HeroSection = () => {
         >
           <h2 className="text-lg text-[#333333] mb-2 font-bold">The World 1st </h2>
           <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-slate-800 mb-6">
-            <span>Staff Sharing</span> Model
+           Staff Sharing Platform
           </h1>
           <div className="flex flex-col sm:flex-row gap-4 justify-center lg:justify-start">
             <Button size="lg" className="bg-[#6CBB2D] shadow-xl hover:bg-blue-700">
